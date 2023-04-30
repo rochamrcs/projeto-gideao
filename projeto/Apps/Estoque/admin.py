a53dc8efffd87525b1e3c2282703110dc708c268
Marcos <mavinicius95@gmail.com>
2023-04-29 21:01:50 -0300

    Testando algumas personalizaçãos no painel admin

--- projeto/Apps/Estoque/admin.py
+++ projeto/Apps/Estoque/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from projeto.Apps.Estoque.models import Posicao
 
-admin.site.register(Posicao)
+class PosicaoAdmin(admin.ModelAdmin):
+    list_display = ('posicao', 'capacidade') #Personaliza a exibição de colunas no painel admin
+    ordering = ['posicao'] #Ordena por posição no painel admin
+
+admin.site.register(Posicao, PosicaoAdmin)
