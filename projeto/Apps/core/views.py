8d39bd0765aa7c82b75b3c699c4d0508d1eab0cb
Marcos <mavinicius95@gmail.com>
2023-05-01 20:41:36 -0300

    Configurando homepage e arquivos estaticos

--- projeto/Apps/core/views.py
+++ projeto/Apps/core/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Olá mundo!,  você está em home')
+    return render(request, './home.html')
--- projeto/settings.py
+++ projeto/settings.py
@@ -62,7 +62,10 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'projeto/templates/global',
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'projeto/base_static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
