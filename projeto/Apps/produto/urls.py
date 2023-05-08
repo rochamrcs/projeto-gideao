eb57337742914f827f92c714b0abeba03b4b3dd9
Marcos <mavinicius95@gmail.com>
2023-05-07 23:01:39 -0300

    Adicionado cadastro de novos produtos

--- projeto/Apps/produto/urls.py
+++ projeto/Apps/produto/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from projeto.Apps.produto import views
 
 urlpatterns = [
-    path('', views.ProdutoView.as_view(), name='produto')
+    path('', views.ProdutoView.as_view(), name='produto'),
+    path('novo_produto/', views.CreateProduto.as_view(), name='novo_produto'),
 ]
--- projeto/Apps/produto/views.py
+++ projeto/Apps/produto/views.py
@@ -1,6 +1,8 @@
 from typing import Any, Dict
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 from .models import Produto
+from .forms import ProdutoForm
+from django.urls import reverse_lazy
 
 
 class ProdutoView(TemplateView):
@@ -10,3 +12,9 @@ class ProdutoView(TemplateView):
         contexto = super().get_context_data(**kwargs)
         contexto["produto_lista"] = Produto.objects.all()
         return contexto
+
+
+class CreateProduto(CreateView):
+    template_name = './produto/novo_produto.html'
+    form_class = ProdutoForm
+    success_url = reverse_lazy('produto')
