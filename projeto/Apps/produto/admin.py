c51d90d5af67c80ce4325e1193f96a81e2f72a8d
Marcos <mavinicius95@gmail.com>
2023-05-08 23:07:01 -0300

    Melhorando as informações de cadastro de produtos

--- projeto/Apps/produto/admin.py
+++ projeto/Apps/produto/admin.py
@@ -4,7 +4,7 @@ from projeto.Apps.produto.models import Produto
 
 
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('produto', 'planta', 'tipo', 'criado_em')
+    list_display = ('produto', 'planta', 'tipo', 'criado_em', 'modificado_em')
     ordering = ['produto']
 
 admin.site.register(Produto, ProdutoAdmin)
--- projeto/Apps/produto/models.py
+++ projeto/Apps/produto/models.py
@@ -12,13 +12,21 @@ TIPO = [
     ('PA', 'PRODUTO ACABADO')
 ]
 
+STATUS = [
+    ('A', 'Ativo'),
+    ('I', 'Inativo')
+]
+
+
 class Produto(models.Model):
     produto = models.CharField(max_length=30, default="", unique=True, error_messages={
         'unique': 'Essa já foi cadastrado.'
     })
     planta = models.CharField(max_length=2, choices=PLANTA, default="Selecione")
     tipo = models.CharField(max_length=2, choices=TIPO, default="Selecione")
+    status = models.CharField(max_length=2, choices=STATUS, default="Ativo")
     criado_em = models.DateTimeField(auto_now_add=True)
+    modificado_em = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.criado_em.strftime('%d/%m/%Y %H:%M:%S')
