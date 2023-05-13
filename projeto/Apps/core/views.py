924c9772a57ed3755f9016710f5ccc2dcea85ed9
Marcos <mavinicius95@gmail.com>
2023-05-13 00:53:18 -0300

    Configurado o login e o logout

--- projeto/Apps/core/views.py
+++ projeto/Apps/core/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
-from django.http import HttpResponse
 
+@login_required
 def home(request):
     return render(request, './home.html')
+
+
+def login(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user =  authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, './home.html' )
+    else:
+        return render(request, './login.html' )
