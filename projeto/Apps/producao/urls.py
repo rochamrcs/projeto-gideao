9aa3aad59fd041d623c5692f6f4909341edc2c11
Marcos <mavinicius95@gmail.com>
2023-05-17 20:26:12 -0300

    Adicionando detalhes da produção

--- projeto/Apps/producao/urls.py
+++ projeto/Apps/producao/urls.py
@@ -3,5 +3,6 @@ from projeto.Apps.producao import views
 
 urlpatterns = [
     path('', views.ProducaoView.as_view(), name='producao'),
-    path('novo_producao/', views.CreateProducao.as_view(), name='nova_producao')
+    path('novo_producao/', views.CreateProducao.as_view(), name='nova_producao'),
+    path('detalhe_producao/<str:pk>/', views.DetailProducao.as_view(), name='detalhe_producao')
 ]
--- projeto/Apps/producao/views.py
+++ projeto/Apps/producao/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, CreateView, UpdateView
+from django.views.generic import TemplateView, CreateView, UpdateView, DetailView
 from .models import Producao
 from .forms import ProducaoForm
 from django.urls import reverse_lazy
@@ -17,3 +17,13 @@ class CreateProducao(CreateView):
     template_name = './producao/nova_producao.html'
     form_class = ProducaoForm
     success_url = reverse_lazy('producao')
+
+
+class DetailProducao(DetailView):
+    template_name = './producao/detalhe_producao.html'
+    model = Producao
+
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        contexto["producao_lista"] = Producao.objects.all()
+        return contexto
