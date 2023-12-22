c5a87f27195e947469e9e7409ff9ba8656b527e9
rochamrcs <mavinicius95@gmail.com>
2023-12-21 22:10:17 -0300

    Apagando app obsoleto

--- projeto/apps/posicoes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from projeto.apps.posicoes.views import posicao
-
-
-urlpatterns = [
-    path('', posicao, name='posicao'),
-]
--- projeto/apps/posicoes/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def posicao(request):
-
-    
-    return render(request, 'posicoes/posicoes.html')
